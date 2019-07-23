88a04862fdd7dfd9401667a05203a055a752968c
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-07-23 17:02:12 +0300

    develop integration test

--- src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './index';
-
-describe('correct guess', () => {
-    test('return an action with type correct_guess', () => {
-        const action = correctGuess();
-        expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-    });
-});
---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import {storeFactory} from '../test/testUtills'
+import {guessWord} from './actions'
+
+describe('guessWord action dispatcher', () => {
+    describe('no guess words', () => {
+        test('updates state correctly for unsucceful guess', () => {
+
+        })
+        test('updates state correctly for succeful guess', () => {
+            
+        })
+    })
+    describe('some guess words', () => {
+        test('updates state correctly for unsucceful guess', () => {
+
+        })
+        test('updates state correctly for succeful guess', () => {
+            
+        })
+    })
+}) 
