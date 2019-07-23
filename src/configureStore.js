591db24f891d11bffe63776f3bf4dd7110348e95
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-07-23 13:12:45 +0300

    thunk added

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from './redux-thunk';
 import rootReducer from './reducers';
+
+export const middlewares = [ReduxThunk];
 
 export default createStore(rootReducer);
